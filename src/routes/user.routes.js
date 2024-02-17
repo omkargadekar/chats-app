8ea6e79dd39f6b0e59507cba552f1aef91d89eb7
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-02-17 14:56:33 +0530

    fix route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,7 @@ import {
   logoutUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { verifyJWT } from "../middlewares/verifyJWT.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
