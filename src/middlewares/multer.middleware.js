735dbbba08f56511a25223c88825311e569479ad
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-02-23 11:58:08 +0530

    add middle

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // This storage needs public/images folder in the root directory
     // Else it will throw an error saying cannot find path public/images
-    cb(null, "./public/images");
+    cb(null, "public/images");
   },
   // Store file in a .png/.jpeg/.jpg format instead of binary
   filename: function (req, file, cb) {
