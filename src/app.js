2a5a4298549970e3763eec3edabdd27c78f69bfb
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-03-05 16:01:27 +0530

    add 3030 again

--- src/app.js
+++ src/app.js
@@ -41,6 +41,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "http://localhost:3030",
       "*",
       "https://chatsapp-nw05.onrender.com",
       "http://ec2-52-206-76-43.compute-1.amazonaws.com:8000",
