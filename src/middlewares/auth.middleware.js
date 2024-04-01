e870f542b3e1af979a4fbba472256d748a543c79
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-04-01 20:37:52 +0530

    add envsysg

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -67,7 +67,7 @@ export const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       //decodes token id
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
 
