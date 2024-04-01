df7667819ea7612e603895a01dbf1f776ee14b0d
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-04-01 20:46:14 +0530

    add bss

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -57,29 +57,25 @@ export const verifyPermission = (roles = []) =>
     }
   });
 export const protect = asyncHandler(async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-
-      //decodes token id
-      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-      req.user = await User.findById(decodedToken?._id).select("-password");
-
-      next();
-    } catch (error) {
-      res.status(401);
-      throw new Error("Not authorized, token failed");
-    }
-  }
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
   }
 });
