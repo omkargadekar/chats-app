2a2bb58158935c383b9f91e01c8047de220cceec
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-04-01 20:48:20 +0530

    afd

--- src/controllers/dashMessage.controller.js
+++ src/controllers/dashMessage.controller.js
@@ -22,7 +22,7 @@ const allMessages = asyncHandler(async (req, res) => {
 //@route           POST /api/Message/
 //@access          Protected
 const sendMessage = asyncHandler(async (req, res) => {
-  const { content, chatId } = req.body;
+  const { userId, content, chatId } = req.body;
 
   if (!content || !chatId) {
     console.log("Invalid data passed into request");
@@ -30,7 +30,7 @@ const sendMessage = asyncHandler(async (req, res) => {
   }
 
   var newMessage = {
-    sender: req.user._id,
+    sender: userId,
     content: content,
     chat: chatId,
   };
