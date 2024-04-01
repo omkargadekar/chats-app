f4b36e3508dd5161e5be328d53351f608244b19d
Omkar <105555236+omkargadekar@users.noreply.github.com>
2024-04-01 21:09:29 +0530

    add bhangar

--- src/controllers/dashMessage.controller.js
+++ src/controllers/dashMessage.controller.js
@@ -22,24 +22,28 @@ const allMessages = asyncHandler(async (req, res) => {
 //@route           POST /api/Message/
 //@access          Protected
 const sendMessage = asyncHandler(async (req, res) => {
-  const { userId, content, chatId } = req.body;
-
-  if (!content || !chatId) {
-    console.log("Invalid data passed into request");
-    return res.sendStatus(400);
-  }
-
-  var newMessage = {
-    sender: userId,
-    content: content,
-    chat: chatId,
-  };
-
   try {
+    const { userId, content, chatId } = req.body;
+
+    console.log(userId, content, chatId);
+
+    if (!content || !chatId) {
+      console.log("Invalid data passed into request");
+      return res.sendStatus(400);
+    }
+
+    var newMessage = {
+      sender: userId,
+      content: content,
+      chat: chatId,
+    };
+
+    console.log(newMessage);
+
     var message = await DashMessage.create(newMessage);
 
-    message = await message.populate("sender", "name pic").execPopulate();
-    message = await message.populate("chat").execPopulate();
+    message = await message.populate("sender");
+    message = await message.populate("chat");
     message = await User.populate(message, {
       path: "chat.users",
       select: "name pic email",
